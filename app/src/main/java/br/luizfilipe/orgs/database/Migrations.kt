7c0f568d1b78abdfb9c90aaef49cbecd57ee3265
Luiz-Filipee <luizkato7@gmail.com>
2024-05-02 16:03:55 -0400

    feat: implementando migrations

---
+++ app/src/main/java/br/luizfilipe/orgs/database/Migrations.kt
@@ -0,0 +1,22 @@
+package br.luizfilipe.orgs.database
+
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+// Todo Todas as alteracoes que fizermos dentro do banco de dados devemos informar por meio
+// Todo das migrations, independente do que for
+
+// Criando uma migration para criar a table de user
+val MIGRATION_1_2 = object : Migration(1, 2) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL(
+            """
+            CREATE TABLE IF NOT EXISTS `User` (
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+            `nome` TEXT NOT NULL, `email` TEXT NOT NULL, 
+            `senha` TEXT NOT NULL, `telefone` TEXT NOT NULL, 
+            `imagem` TEXT)"""
+        )
+    }
+
+}
